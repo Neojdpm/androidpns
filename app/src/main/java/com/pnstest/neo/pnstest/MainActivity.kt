f4769f5cac3c39ce1a43ef7e731b7864981a77dd
Jose <neojdpm@gmail.com>
2019-03-25 12:35:29 -0400

    Release 0.5

--- app/src/main/java/com/pnstest/neo/pnstest/MainActivity.kt
+++ app/src/main/java/com/pnstest/neo/pnstest/MainActivity.kt
@@ -6,11 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.pnstest.neo.androidpnsconnection.AndroidPNSConnection
-import com.google.firebase.internal.FirebaseAppHelper.getToken
-import com.google.firebase.iid.InstanceIdResult
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.firebase.iid.FirebaseInstanceId
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AndroidPNSConnection("https://dev.panatech.io/","https://pns.dev.panatech.io:3003/","asd", "asd", 2)
     }
 }
